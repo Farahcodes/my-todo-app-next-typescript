fad27f02f3887a33ea0ff010cd340c34f6d975b4
farah <farah.fournat@gmail.com>
2022-11-13 17:29:10 +0100

    Sidebar List

--- pages/Sidebar.tsx
+++ pages/Sidebar.tsx
@@ -2,11 +2,10 @@ import { ReactComponentElement, useState } from "react";
 import styles from "../styles/Sidebar.module.css";
 import { FaPlus } from "react-icons/fa";
 import SideBarList from "./SideBarList";
-import { FaEdit, FaTrash, FaCheck } from "react-icons/fa";
 
 interface Lists {
   lists: Array<List>;
-  name: string;
+  title: string;
   id: number;
 }
 interface List {
@@ -18,9 +17,10 @@ const Sidebar = () => {
   const [listOfLists, setListOfLists] = useState<Array<Lists>>([]);
 
   function handleAdd(e: React.MouseEvent<HTMLInputElement>) {
-    setListOfLists(
-      listOfLists.concat(<SideBarList key={listOfLists.length} />)
+    const newList = listOfLists.concat(
+      <SideBarList key={listOfLists.length} />
     );
+    setListOfLists(newList);
   }
 
   return (
@@ -37,33 +37,10 @@ const Sidebar = () => {
 
       <div className={styles.listsContainer}>
         {listOfLists.map((item) => {
-          const { name, id } = item;
+          const { title, id } = item;
           return (
             <article key={id} className={styles.newList}>
-              <SideBarList name="New List" />
-              <div className={styles.container}>
-                <button
-                  type="button"
-                  className="styles.editBtn"
-                  onClick={() => editList(id)}
-                >
-                  <FaEdit />
-                </button>
-                <button
-                  type="button"
-                  className="styles.deleteBtn"
-                  onClick={() => removeList(id)}
-                >
-                  <FaTrash />
-                </button>
-                <button
-                  type="button"
-                  className="styles.completeBtn"
-                  onClick={() => completeList(id)}
-                >
-                  <FaCheck />
-                </button>
-              </div>
+              <SideBarList title={title} id={id} />
             </article>
           );
         })}
