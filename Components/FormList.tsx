fe41d0f35952aee9938eea9b4d4a8b233d61d5cd
farah <farah.fournat@gmail.com>
2022-12-06 21:27:49 +0100

    added Alert to FormList

--- Components/FormList.tsx
+++ Components/FormList.tsx
@@ -1,19 +1,44 @@
 import { ReactComponentElement, useState } from "react";
 import styles from "../styles/Sidebar.module.css";
 import { FaPlus } from "react-icons/fa";
+import Alert from "./Alert";
+import { List } from "./Sidebar";
 
-const FormList = ({ handleAdd }: { handleAdd: (title: string) => void }) => {
+const FormList = ({
+  handleAdd,
+  itemList,
+}: {
+  handleAdd: (title: string) => void;
+  itemList: List;
+}) => {
   const [title, setTitle] = useState<string>("e.g. Grocery");
+  const [alert, setAlert] = useState<any>({
+    show: false,
+    msg: "",
+    type: "",
+  });
+
+  function showAlert(show = false, type = "", msg = "") {
+    setAlert({ show, type, msg });
+  }
 
   function handleChange(e: React.FormEvent<HTMLInputElement>) {
     setTitle(e.currentTarget.value);
   }
   function onClick() {
-    handleAdd(title);
+    if (!title) {
+      showAlert(true, "danger", "please enter value");
+    } else if (title) {
+      showAlert(true, "success", "new list added");
+      handleAdd(title);
+    }
   }
 
   return (
     <div className={styles.container}>
+      {alert.show && (
+        <Alert {...alert} removeAlert={showAlert} itemList={itemList} />
+      )}
       <input
         type="text"
         className={styles.input}
